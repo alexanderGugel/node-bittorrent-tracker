80397520734c81babaf50004c194e8e79a6e9823
Alexander Gugel <alexander.gugel@gmail.com>
2014-05-18 17:30:30 +0200

    Use env for ports; Rename example.js -> tracker.js

--- example.js
+++
@@ -1,6 +0,0 @@
-var tracker = require("./lib/tracker");
-
-var t = tracker.Tracker();
-
-tracker.udp.createServer(t, 8080);
-tracker.http.createServer(t, 8080);
---
+++ tracker.js
@@ -0,0 +1,9 @@
+var tracker = require("./lib/tracker");
+
+var t = tracker.Tracker();
+
+var udpPort = process.env.UDP_TRACKER_PORT || 8080;
+var httpPort = process.env.HTTP_TRACKER_PORT || 8080;
+
+tracker.udp.createServer(t, udpPort);
+tracker.http.createServer(t, httpPort);
